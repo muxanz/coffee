2098a309d08132909affa85510326b3c5328cd0b
Samuel Florez <muxanzz@gmail.com>
2024-07-18 15:46:57 -0500

    add the file gulpfile.js and create the first task

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const { src, dest } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function css(done) {
+   /*
+   COMPILAR SASS
+   1. identificar archivo, 2. compilar, 3. guardar el .css
+   */
+   src('src/scss/app.scss')
+      .pipe(sass())
+      .pipe(dest('build/css'))
+
+   done();
+}
+
+exports.css = css;
