774b576871ab8f503b83e0bf4c426d8695861cd5
Samuel Florez <muxanzz@gmail.com>
2024-07-23 10:15:27 -0500

    add src/img structure and create functions to optimize images in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 const { src, dest, watch, series, parallel } = require('gulp');
+
+// css y sass
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+
+// imagenes
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done) {
    /*
@@ -16,11 +23,43 @@ function css(done) {
    done();
 }
 
+function imagenes(done) {
+   src('src/img/**/*')
+      .pipe(imagemin())
+      .pipe(dest('build/img'));
+
+   done();
+}
+
+function versionWebp() {
+   const opciones = {
+      quality: 50
+   }
+
+   return src('src/img/**/*.{png,jpg}')
+      .pipe(webp(opciones))
+      .pipe(dest('build/img'))
+}
+
+function versionAvif() {
+   const opciones = {
+      quality: 50
+   }
+
+   return src('src/img/**/*.{png,jpg}')
+      .pipe(avif(opciones))
+      .pipe(dest('build/img'))
+}
+
 function dev() {
    watch('src/scss/**/*.scss', css);
+   watch('src/img/**/*', imagenes);
 }
 
 exports.css = css;
 exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
 //tareas por default
-exports.default = series(css, dev);
+exports.default = series(imagenes, versionWebp, versionAvif, css, dev);
